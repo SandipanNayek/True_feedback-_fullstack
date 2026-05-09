428e3c75cf71e970a4398a085f44e4319f950994
SANDIPAN NAYEK <sandipannayek09@gmail.com>
2026-05-10 01:56:47 +0530

    fix: remove trailing newline at end of User.ts

--- mode/User.ts
+++ mode/User.ts
@@ -73,5 +73,4 @@ const UserSchema: Schema<User> = new Schema({
 
 const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema);
 
-
 export default UserModel;
