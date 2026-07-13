331c47b59daac23d71eff943e22facfe83006f6f
SANDIPAN NAYEK <sandipannayek09@gmail.com>
2026-07-14 01:57:43 +0530

    feat: implement username uniqueness check and sign-up functionality

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,17 +3,69 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
 import Link from "next/link"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useDebounceValue} from 'usehooks-ts'
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
+import { signUpSchema } from "@/schemas/signUpSchema"
+import axios, {AxiosError} from 'axios'
+import { ApiResponse } from "@/types/ApiResponse"
+
 
 const Page = () => {
     const [username, setUsername] = useState('')
     const [usernameMessage, setUsernameMessage] = useState('')
     const [isCheckingUsername, setIsCheckingUsername] = useState(false)
-    const [isSubmatting, setIsSubmatting] = useState(false)
+    const [isSubmitting, setIsSubmitting] = useState(false)
 
-    const debouncedUsername = useDebounceValue(username, 300)
+    const [debouncedUsername] = useDebounceValue(username, 300)
+    const router = useRouter()
 
+    // zod implementation
+
+    const form = useForm<z.infer<typeof signUpSchema>>({
+      resolver: zodResolver(signUpSchema),
+      defaultValues:{
+        username:"",
+        email:"",
+        password:""
+      }
+    })
+
+    useEffect(()=>{ 
+       const checkUsernameUnique = async () => {
+         if (debouncedUsername) {
+           setIsCheckingUsername(true)
+           setUsernameMessage('')
+           try{
+            const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`)
+            setUsernameMessage(response.data.message)
+           }catch(error){
+              const axiosError = error as AxiosError<ApiResponse>;
+              setUsernameMessage(
+                axiosError.response?.data.message ?? "Error checking username"
+              )
+           } finally{
+            setIsCheckingUsername(false)
+           }
+         }
+       }
+       checkUsernameUnique()
+    },[debouncedUsername])
+
+    const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+        setIsSubmitting(true)
+        try {
+        const response =   await axios.post<ApiResponse>('/api/sign-up' , data)
+        toast.success("Success", {
+          description: response.data.message,
+          duration: 3000,
+        });
+        router.replace(`/verify/${username}`)
+        } catch (error) {
+          
+        }
+    }
 
   return (
     <div>page</div>
--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -8,7 +8,7 @@ export const userNameValidation = z
 
 
 
-export const sinngnUpSchema = z.object({
+export const signUpSchema = z.object({
     username : userNameValidation,
     email: z.string().email({message: 'Invalid email address'}),
     password:z.string().min(8, {message:'Password must be at least 8 characters long'})
